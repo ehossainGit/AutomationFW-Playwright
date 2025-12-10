2be94d3e9494f38a34fba22f6a74e318ef48cedf
ehossain84 <emd84@hotmail.com>
2025-12-10 10:28:47 -0500

    u

--- tests/amazon/OpenApp.spec.ts
+++ tests/amazon/OpenApp.spec.ts
@@ -14,7 +14,8 @@ import  { test, expect } from '@playwright/test';
 await page.getByLabel('Select the department you').selectOption('Books');
 await page.getByRole('searchbox', { name: 'Search Amazon' }).click();
 await page.getByRole('searchbox', { name: 'Search Amazon' }).fill('automation');
-const text = await page.getByRole('button', { name: /automation/ }).textContent();
+// const text = await page.getByRole('button', { name: /automation/ }).textContent();
+const text = await page.locator('//div[@aria-label="automation"]').textContent();
 
 // const text = await page.getByRole('button', { name: 'laptop computer' }).textContent();
 // const text1 = await page.getByRole('button', { name: 'laptop computer' }).getAttribute('aria-label');
